9be3f947bcada994fcdab2d0a29b12d0132c0629
manishapal06 <palmannu003@gmail.com>
2025-05-05 12:19:52 +0530

    added script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'buuuuuuuu......'
+let students=[
+  { name: "Emily", average: 91.67 },
+  { name: "Daniel", average: 72.67 },
+  { name: "Sarah", average: 58.00 },
+  { name: "Alex", average: 85.00 }
+];
+let topStudents=[];
+for(let i=0;i<students.length;i++){
+  if(students[i].average>80){
+    topStudents.push(students[i].name)
+  }
+}
+console.log(topStudents)
