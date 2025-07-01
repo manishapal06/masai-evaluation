4b16c3575f9dca374d289880dd4ff6ba0dba477d
manishapal06 <palmannu003@gmail.com>
2025-07-01 13:11:17 +0530

    evelu

--- demo.js
+++
@@ -1,8 +0,0 @@
-function timer(duration,callback){
-  setTimeout(()=>{
-    callback(`Timer of duration ${duration} ms finished`);
-  },duration);
-}
-timer(2000,(message)=>{
-  console.log(message);
-});
